85484212b4fd104099ee2424d7ef869fd0401f6d
Oltiz <42326377+CodeVinc@users.noreply.github.com>
2021-03-05 14:11:50 -0400

    Update player.js

--- player.js
+++ player.js
@@ -3,63 +3,58 @@ const player = new Plyr('video');
 //const url = new URLSearchParams(document.location.search.substring(1));
 const source = document.location.search.substring(6)
 
-if(source != null) {
+if(source != "" && source.length > 1) 
+{
+    document.addEventListener('DOMContentLoaded', () => {
 
-  document.addEventListener('DOMContentLoaded', () => {
+        const video = document.querySelector('video');
 
-    const video = document.querySelector('video');
-    
-    if (!Hls.isSupported()) {
-      video.src = source;
-    } else {
-      // For more Hls.js options, see https://github.com/dailymotion/hls.js
-      const hls = new Hls();
-      hls.loadSource(source);
-      hls.attachMedia(video);
-      window.hls = hls;
-    }
-
-    //Quando pronto.
-    player.on('ready', event => {
-      if(event) {
-        document.getElementById('ads-conteiner').style.display = "flex";
-      }
-    });
-    
-    //Quando assistindo.
-    player.on('play', event => {
-      if(event) {
-        document.getElementById('ads-conteiner').style.display = "none";
-      }
-    });
-  
-    //Quando pausado.
-    player.on('pause', event => {
-        if(event) {
-          document.getElementById('ads-conteiner').style.display = "flex";
+        if (!Hls.isSupported()) {
+            video.src = source;
+        } else {
+            // For more Hls.js options, see https://github.com/dailymotion/hls.js
+            const hls = new Hls();
+            hls.loadSource(source);
+            hls.attachMedia(video);
+            window.hls = hls;
         }
-    });
-      
-    //tratamento de erro.
-    player.on('error', event => {
-      if(event) {
-        alert('Não foi possivel reproduzir este video!');
-      }
-    });
-    
-    //video ads
-    const adsClose = document.getElementById("ads-close");
-    
-    adsClose.addEventListener('click', function() {
-      document.getElementById('ads-conteiner').style.display = "none";
-      player.play();
-    });
 
-    
-    // Expose player so it can be used from the console
-    window.player = player;
+        //Quando pronto.
+        player.on('ready', event => {
+            if(event) {
+            document.getElementById('ads-conteiner').style.display = "flex";
+            }
+        });
 
-  });
+        //Quando assistindo.
+        player.on('play', event => {
+            if(event) {
+            document.getElementById('ads-conteiner').style.display = "none";
+            }
+        });
+
+        //Quando pausado.
+        player.on('pause', event => {
+            if(event) {
+                document.getElementById('ads-conteiner').style.display = "flex";
+            }
+        });
+            
+        //tratamento de erro.
+        player.on('error', event => {
+            if(event) {
+                alert('Não foi possivel reproduzir este video!');
+            }
+        });
+
+        //video ads
+        const adsClose = document.getElementById("ads-close");
+
+        adsClose.addEventListener('click', function() {
+            document.getElementById('ads-conteiner').style.display = "none";
+            player.play();
+        });
+    });
 } else {
-  alert('Não foi encontado nenhum video para reproduzir!');
+    alert('Não foi encontado nenhum video para reproduzir.');
 }
